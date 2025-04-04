e5e86001e1a9ab2203a955d385fcaaff4faa4ea9
Gustavo Fernandez Pascoaleto <106634494+GustavoAlot@users.noreply.github.com>
2025-04-04 16:04:41 -0300

    Update importar_dados.sql

--- banco_dados/importar_dados.sql
+++ banco_dados/importar_dados.sql
@@ -1,8 +1,8 @@
--- Importação das operadoras
+-- Import das operadoras
 \copy operadoras(registro_ans, cnpj, razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, uf, cep, ddd, telefone, fax, endereco_eletronico, representante, cargo_representante, regiao_comercializacao, data_registro_ans) FROM 'banco_dados/dados/operadoras.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8', QUOTE '"');
 
 ------------------------------------------------------------
--- Importação dos arquivos trimestrais para a tabela staging
+-- Import dos arquivos trimestrais para a tabela staging
 
 -- 1º Trimestre de 2023
 \copy demonstracoes_contabeis_staging(data, reg_ans, cd_conta_contabil, descricao, vl_saldo_inicial, vl_saldo_final) FROM 'banco_dados/dados/1T2023.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
@@ -29,7 +29,7 @@
 \copy demonstracoes_contabeis_staging(data, reg_ans, cd_conta_contabil, descricao, vl_saldo_inicial, vl_saldo_final) FROM 'banco_dados/dados/4T2024.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
 
 ------------------------------------------------------------
--- Consolidação dos dados da tabela staging na tabela final
+-- consolidacao dos dados da tabela staging na tabela final
 
 INSERT INTO demonstracoes_contabeis (
     operadora_id, data, ano, trimestre, descricao, vl_saldo_inicial, vl_saldo_final, reg_ans
